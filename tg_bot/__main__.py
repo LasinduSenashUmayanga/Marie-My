24479e3e10e64b91bc3f222bc95dc82e87621715
LasinduSenashUmayanga <99899045+LasinduSenashUmayanga@users.noreply.github.com>
2022-02-18 04:28:00 +0000

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,18 +18,15 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
+Hᴇʟʟᴏ {}, Mʏ Nᴀᴍᴇ Is Sᴀʟʟʏ.. 🌷 
 
-im a group manager bot maintained by  [this person](tg://user?id={}).
+Iғ Yᴏᴜ Hᴀᴠᴇ Aɴʏ Qᴜᴇsᴛɪᴏɴs Aʙᴏᴜᴛ Hᴏᴡ Tᴏ Usᴇ Mᴇ Pʟᴇᴀsᴇ Gɪᴠᴇ Mᴇ /help... 
 
-My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
+Iᴍ A Gʀᴏᴜᴘ Mᴀɴᴀɢᴇʀ Bᴏᴛ Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ  [Kɪɴɢ Aᴍᴅᴀ.](t.me/NiupunDinujaya)
 
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
-you can create clone same like me..
+Mʏ Fᴜᴛᴜʀᴇ Uᴘᴅᴀᴛᴇs Wɪʟʟ Bᴇ Pᴜᴛ Iɴᴛᴏ Tʜɪs Cʜᴀɴɴᴇʟ - [@TʜᴇSᴀʟʟʏUᴘᴅᴀᴛᴇs](t.me/TheSallyUpdates) & Mʏ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ [@TʜᴇSᴀʟʟʏSᴜᴘᴘᴏʀ.](t.me/TheSallySupport)
 
-For more commands click /help...
-
-**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
+Fᴏʀ Mᴏʀᴇ Cᴏᴍᴍᴀɴᴅs Cʟɪᴄᴋ /ʜᴇʟᴘ...
 
 """
 
@@ -50,9 +47,9 @@ And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll of the following commands  / or ! can  be used...\n")
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
+It took lots of work for [my creator](t.me/NiupunDinujaya) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
-(see his bio!). He's just a poor student, so every little helps!
+). He's just a poor student, so every little helps!
 There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
 
 IMPORTED = {}
